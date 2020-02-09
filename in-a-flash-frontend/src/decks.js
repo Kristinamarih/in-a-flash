b4e0ed65fa7f84e0320c5c458a0229bacff358ec
Kristinamarih <kristinahorensky@gmail.com>
2020-02-09 15:30:01 -0800

    adds classes to decks.js

--- in-a-flash-frontend/src/decks.js
+++ in-a-flash-frontend/src/decks.js
@@ -1,9 +1,49 @@
-class DecksAdapter {
+class DeckAdapter {
     constructor() {
-        this.baseURL = 'http://localhost:300/decks'
+        this.baseURL = 'http://localhost:3000/decks'
     }
 
-    getDecks() {
+    getDeck() {
         return fetch(this.baseURL).then(res => res.json())
+    }
+}
+
+class Deck {
+    constructor(deckJSON) {
+        this.id = deckJSON.id 
+        this.name = deckJSON.name 
+        this.category = deckJSON.category
+    }
+
+    renderLi() {
+        return `<li>${this.name}</li>`
+    }
+}
+
+class Decks {
+    constructor() {
+        this.decks = []
+        this.adapter = new DecksAdapter()
+        this.bindEventListeners()
+        this.fetchAndLoadDecks()
+    }
+
+    bindEventListeners() {
+        this.decksContainer = document.getElementById('decks-container')
+    }
+
+    fetchAndLoadDecks() {
+        this.adapter
+        .getDecks()
+        .then(decks => {
+            decks.forEach(deck => this.decks.push(new Deck(deck)))
+        })
+        .then(() => {
+            this.render()
+        })
+    }
+
+    render() {
+        decksContainer.innerHTML = this.decks.map(deck => deck.renderLi().join(''))
     }
 }
