8e6138026b39a45e7b918a11e00be08a82b7d167
Kristinamarih <kristinahorensky@gmail.com>
2020-02-08 17:41:44 -0800

    built out deck controller

--- in-a-flash-backend/app/controllers/decks_controller.rb
+++ in-a-flash-backend/app/controllers/decks_controller.rb
@@ -1,2 +1,52 @@
 class DecksController < ApplicationController
+
+    def index
+        decks = Deck.all 
+        render json: decks
+    end
+
+    def show
+        deck = Deck.find_by(id: params[:id])
+        render json: deck
+    end
+
+    def new
+        deck = Deck.new
+    end
+
+    def create
+        deck = Deck.new(deck_params)
+        if deck.save
+            render json: deck
+        else
+            render: new
+        end
+    end
+
+    def edit
+        deck = Deck.find_by(params["id"])
+        render: edit
+    end
+
+    def update
+        deck = Deck.find_by(params["id"])
+        if deck.update(deck_params)
+            redirect_ to deck_path(deck)
+        else
+            render: edit
+        end
+    end
+
+    def destroy
+        deck = Deck.find_by(params["id"])
+        deck.delete
+        redirect_to deck_path
+    end
+
+    private
+    
+    def deck_params
+        params.require(:deck).require(:name, :category)
+    end
+
 end
