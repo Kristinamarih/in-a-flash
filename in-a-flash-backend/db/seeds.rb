3d4af7235030ed6f530cfc66011af2a51e0c356a
Kristinamarih <kristinahorensky@gmail.com>
2020-02-18 14:29:15 -0800

    add seed data

--- in-a-flash-backend/db/seeds.rb
+++ in-a-flash-backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Deck.create(name: 'Norsk101', category: 'Languages')
+
