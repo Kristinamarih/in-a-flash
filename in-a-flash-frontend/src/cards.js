e976eb3a0a022e5d72fcde6eddb2299af3ad6eb5
Kristinamarih <kristinahorensky@gmail.com>
2020-09-25 17:03:44 -0700

    fixes button size

--- in-a-flash-frontend/src/cards.js
+++ in-a-flash-frontend/src/cards.js
@@ -12,16 +12,18 @@ class Card {
     };
 
     renderCard() {
-        return `<div class="card border-primary mb-3" style="max-width: 30em;">
+        return `<div class="card border-primary mb-3" style="max-width: 30em">
         <div class="card-header">Card</div>
                     <div class="card-body text-center">
                         <h4 class="card-title">${this.term}</h4>
                         <p class="card-text">${this.description}</p>
                     </div>
-                    <div class="btn-group" role="group" style="width: 33% text-align: center">
-                    <button data-id=${this.id} type="button" id="previous-card" class="btn btn-outline-primary">Previous</button>
-                    <button data-id=${this.id} type="button" id="delete-card" class="btn btn-primary">Delete</button>
-                    <button data-id=${this.id} type="button" id="next-card" class="btn btn-outline-primary">Next</button>
+                    <div class="text-align: center">
+                        <div class="btn-group" role="group">
+                        <button data-id=${this.id} type="button" id="previous-card" style="width: 7em" class="btn btn-outline-primary">Previous</button>
+                        <button data-id=${this.id} type="button" id="delete-card" style="width: 7em" class="btn btn-primary">Delete</button>
+                        <button data-id=${this.id} type="button" id="next-card" style="width: 7em" class="btn btn-outline-primary">Next</button>
+                        </div>
                     </div>
                 </div>`
     };
